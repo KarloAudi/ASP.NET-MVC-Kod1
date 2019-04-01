3934534a4749b126f3d502d1f92ee5297f43dffe
KarloAudi <48256124+KarloAudi@users.noreply.github.com>
2019-04-01 17:44:26 +0200

    Code

---
+++ BindTablesClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestProject9.Models
+{
+    public class BindTablesClass
+    {
+        public VehicleMake makeDetails { get; set; }
+        public VehicleModel modelDetails { get; set; }
+    }
+}
---
+++ VehicleController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestProject9.Models;
+using PagedList.Mvc;
+using PagedList;
+using System.Net;
+using System.Data.Entity;
+
+namespace TestProject9.Controllers
+{
+    public class VehicleController : Controller
+    {
+        VehiclesEntities db = new VehiclesEntities();
+        // GET: Vehicle
+        public ActionResult Index(string searching, int? i)
+        {
+            List<VehicleMake> makeListing = db.VehicleMakes.ToList();
+            List<VehicleModel> modelListing = db.VehicleModels.ToList();
+
+            var joinTables = from vhMake in makeListing
+                             join vhModel in modelListing on vhMake.Id equals vhModel.MakeId into table1
+                             from vhModel in table1.DefaultIfEmpty()
+                             select new BindTablesClass { makeDetails = vhMake, modelDetails = vhModel };
+
+            if (!String.IsNullOrEmpty(searching))
+            {
+                joinTables = joinTables.Where(vhMake => vhMake.makeDetails.Name.Contains(searching));
+            }
+
+            return View(joinTables.ToList().ToPagedList(i ?? 1, 10));
+        }
+    }
+}
